516d91c04771186de795258ce5e775f4bbfd3f91
toksuz <toksuz@nuevo.com.tr>
2018-03-22 14:56:18 +0300

    application create

---
+++ app/src/main/java/tr/com/nuevo/dagger_rxandroid_database_mvp/application/CakeApplication.java
@@ -0,0 +1,19 @@
+package tr.com.nuevo.dagger_rxandroid_database_mvp.application;
+
+import android.app.Application;
+
+/**
+ * Created by nuevomacpro13 on 22.03.2018.
+ */
+
+public class CakeApplication extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+}
